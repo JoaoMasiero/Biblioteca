37dbf2d81fb9d4e187d6e3d0a2d2d04f95724a3d
JoaoMasiero <joaomasiero007@gmail.com>
2025-11-03 19:15:11 -0300

    .

--- Biblioteca/Models/Editora.cs
+++ Biblioteca/Models/Editora.cs
@@ -9,10 +9,10 @@ namespace Biblioteca.Models
         [Required(ErrorMessage = "O nome da editora é obrigatório!")]
         [DisplayName("Nome")]
         public string EditoraNome { get; set;}
-        [Required(ErrorMessage = "A cidade da Editora é obrigatório!")]
+        [Required(ErrorMessage = "A cidade da editora é obrigatório!")]
         public string Cidade { get; set; }
         [Required(ErrorMessage = "O telefone da editora é obrigatório!")]
-        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "O e-mail informado não é válido!")]
+        [RegularExpression(@"\(\d{2}\) \d{5}-\d{4}", ErrorMessage = "O telefone deve estar no formato (99) 99999-9999")]
         public int Telefone { get; set; }
         public ICollection<Publicacao> Publicacoes { get; set; } = new List<Publicacao>();
     }
